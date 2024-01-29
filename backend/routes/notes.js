21c5d7efd6a265945eb96e9ae2167f3ab026bb5a
ishaan001 <ishaanveerdadhwal@gmail.com>
2024-01-29 02:52:19 +0100

    update notes feature added

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -52,4 +52,44 @@ router.post(
   }
 );
 
+//ROUTE 3: update a note using: POST "/api/auth/updatenote". login required
+router.put("/updatenote/:id", fetchuser, [
+  body("title", "Enter a valid password with 5 character min")
+    .notEmpty()
+    .isLength({ min: 5 }),
+  body("description", "Enter a valid password with 8 character min")
+    .notEmpty()
+    .isLength({ min: 8 }),
+],async (req, res) => {
+  try {
+    //console.log('ID:', req.params.id);
+    const { title, description, tag } = req.body;
+    //new note object
+    const newNote = {};
+    if(title) {newNote.title = title}
+    if(description) {newNote.description = description}
+    if(tag) {newNote.tag = tag}
+    //console.log("newNote:", newNote);
+    //find the note to be updated and then update it
+    let note = await Notes.findById(req.params.id);
+    //console.log('Note:', note);
+
+    if(!note){
+      return res.status(404).send("not found")
+    }
+
+    if(note.user.toString() != req.user.id) {
+      return res.status(401).send("not allowed")
+    }
+    note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true});
+    //console.log('Updated Note:', note);
+    res.json({note});
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal server error");
+  }
+});
+
+//ROUTE 4: delete a note using: POST "/api/auth/deletenote". login required
+
 module.exports = router;
